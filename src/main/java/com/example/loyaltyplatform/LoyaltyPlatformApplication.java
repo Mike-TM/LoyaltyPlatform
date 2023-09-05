109460dfad0017bf83fce0ce289e1d31562793aa
Mike-TM <michele.casadidio@studenti.unicam.it>
2023-09-05 14:02:30 +0200

    Initial commit

---
+++ src/main/java/com/example/loyaltyplatform/LoyaltyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.loyaltyplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoyaltyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loyaltyplatform/LoyaltyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loyaltyplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
