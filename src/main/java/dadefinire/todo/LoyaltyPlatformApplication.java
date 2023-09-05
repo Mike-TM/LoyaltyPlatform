e9494c325bd2aa62da67ee26283bb07dbda29657
Giulia Gatti <giuliagatti2000@gmail.com>
2023-09-05 18:41:45 +0200

    prova refactor package

--- src/main/java/dadefinire/todo/LoyaltyPlatformApplication.java
+++ src/main/java/dadefinire/todo/LoyaltyPlatformApplication.java
@@ -1,4 +1,4 @@
-package it.unicam.cs.ids.loyaltyplatform;
+package dadefinire.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/test/todo/LoyaltyPlatformApplicationTests.java
+++ src/test/java/test/todo/LoyaltyPlatformApplicationTests.java
@@ -1,4 +1,4 @@
-package it.unicam.cs.ids.loyaltyplatformtest;
+package test.todo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
