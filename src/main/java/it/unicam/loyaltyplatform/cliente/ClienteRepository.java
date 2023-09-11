733a23dded3d32e0d45ee8cb619d177256e3d02c
Michele Casadidio <michele.casadidio@studenti.unicam.it>
2023-09-11 16:01:03 +0200

    aggiunte dipendenze a build.gradle per la connessione con mysql

---
+++ src/main/java/it/unicam/loyaltyplatform/cliente/ClienteRepository.java
@@ -0,0 +1,6 @@
+package it.unicam.loyaltyplatform.cliente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
