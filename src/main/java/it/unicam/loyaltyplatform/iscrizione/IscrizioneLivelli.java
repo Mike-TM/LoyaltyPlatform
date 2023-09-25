e308dc206c852673015cae509e65b36bc4380a1a
Giulia Gatti <giuliagatti2000@gmail.com>
2023-09-25 14:39:45 +0200

    aggiunta entità che rappresenta iscrizione ad un programma fedeltà a livelli

--- src/main/java/it/unicam/loyaltyplatform/iscrizione/IscrizioneLivelli.java
+++ src/main/java/it/unicam/loyaltyplatform/iscrizione/IscrizioneLivelli.java
@@ -1,10 +1,34 @@
 package it.unicam.loyaltyplatform.iscrizione;
 
-import it.unicam.loyaltyplatform.cliente.Cliente;
+import it.unicam.loyaltyplatform.premio.Premio;
 import it.unicam.loyaltyplatform.programmaFedelta.ProgrammaFedelta;
+import it.unicam.loyaltyplatform.tessera.Tessera;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class IscrizioneLivelli {
+@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "iscrizionelivelli")
+public class IscrizioneLivelli extends Iscrizione{
 
+    @Column
+    private int livelloCorrente;
 
+    @Column
+    private double progressoLivello;
+
+   @OneToMany(mappedBy = "iscrizionelivelli")
+    private List<Premio> premiRiscattati;
+
+   public IscrizioneLivelli(ProgrammaFedelta programmaFedelta, Tessera tessera){
+       super(programmaFedelta,tessera);
+       this.livelloCorrente=0;
+       this.progressoLivello=0.0;
+       this.premiRiscattati=new ArrayList<>();
+   }
 }
