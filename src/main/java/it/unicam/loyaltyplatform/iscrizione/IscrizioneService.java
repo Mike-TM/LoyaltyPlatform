66a9b893727737921c5c04c4264d2d6f3b42aeb5
Giulia Gatti <giuliagatti2000@gmail.com>
2023-09-25 16:31:50 +0200

    implementata convalida acquisto per tipo di iscrizione a livelli

--- src/main/java/it/unicam/loyaltyplatform/iscrizione/IscrizioneService.java
+++ src/main/java/it/unicam/loyaltyplatform/iscrizione/IscrizioneService.java
@@ -19,12 +19,14 @@ public class IscrizioneService {
     private final IscrizioneRepository iscrizioneRepository;
     private final TesseraService tesseraService;
     private final ProgrammaFedeltaService programmaService;
+    private final FactoryIscrizione factoryIscrizione;
 
     @Autowired
-    public IscrizioneService(IscrizioneRepository iscrizioneRepository, TesseraService tesseraService, ProgrammaFedeltaService programmaService) {
+    public IscrizioneService(IscrizioneRepository iscrizioneRepository, FactoryIscrizione factoryIscrizione, TesseraService tesseraService, ProgrammaFedeltaService programmaService) {
         this.iscrizioneRepository = iscrizioneRepository;
         this.tesseraService = tesseraService;
         this.programmaService = programmaService;
+        this.factoryIscrizione=factoryIscrizione;
     }
 
     @GetMapping
@@ -42,10 +44,9 @@ public class IscrizioneService {
 
     @PostMapping
     public void registraIscrizione(Long programmaId, Long tesseraId) throws RecordNotFoundException {
-
         Tessera tessera = tesseraService.findTesseraById(tesseraId);
         ProgrammaFedelta programma = programmaService.findProgrammaByID(programmaId);
-        Iscrizione newIscrizione = new Iscrizione(programma, tessera);
+        Iscrizione newIscrizione = factoryIscrizione.creaIscrizione(programma,tessera);
         programma.getIscrizioni().add(newIscrizione);
         tessera.getIscrizioni().add(newIscrizione);
         iscrizioneRepository.save(newIscrizione);
@@ -55,12 +56,25 @@ public class IscrizioneService {
     @PutMapping@ResponseStatus(value = HttpStatus.OK, reason = "Iscrizione aggiornata")
     public void aggiornaIscrizione(Long idAzienda, Long idTessera) throws RecordNotFoundException{
 
-        iscrizioneRepository.saveAll(tesseraService.findTesseraById(idTessera).getIscrizioni().stream()
+        List<Iscrizione> daAggiornare = tesseraService.findTesseraById(idTessera).getIscrizioni().stream()
                 .filter(i -> i.getProgramma().getAzienda().getAziendaId().equals(idAzienda))
-                .toList());
-        //.forEach aggiorna progresso e poi save
-
+                .toList();
+        daAggiornare.stream().
+                forEach(i -> this.aggiungiProgresso(i));
     }
+
+
+    public void aggiungiProgresso(Iscrizione iscrizione){
+        if(iscrizione instanceof IscrizioneLivelli) aggiungiEsperienzaLivello((IscrizioneLivelli) iscrizione);
+        //if(iscrizione instanceof altroTipoDiIscrizione) doSomething()
+    }
+
+
+    private void aggiungiEsperienzaLivello(IscrizioneLivelli iscrizioneLivelli) {
+        //prendo costanti definite nel programma fedeltà a livelli per decidere esperienza da aggiungere e
+        //controllare se salire di livello
+    }
+
 
     @DeleteMapping
     public void cancellaIscrizione(Long id) throws RecordNotFoundException{
