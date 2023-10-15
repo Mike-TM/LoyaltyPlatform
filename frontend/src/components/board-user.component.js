1822d185192487398dad2f7baf8182dd9731322e
Giulia Gatti <giuliagatti2000@gmail.com>
2023-10-15 19:09:55 +0200

    altre prove per visualizzare lista di iscrizioni

--- frontend/src/components/board-user.component.js
+++ frontend/src/components/board-user.component.js
@@ -2,57 +2,54 @@
 import AuthService from "../services/auth.service";
 import React from 'react';
 import UserService from '../services/user.service';
+import {render} from "@testing-library/react";
+import axios from "axios";
 
 export default class UserComponent extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
-            content:"",
             iscrizioni: []
-        }
+        };
     }
 
-    componentDidMount(){
-        const cliente=AuthService.getCurrentUser();
-        const idCliente=cliente.id;
-        const tessera=UserService.getTesseraByClienteId(idCliente);
-        UserService.getTesseraByClienteId(idCliente).then((response) => {
-            this.setState({ tessera: response.data})
-        });
 
+    componentDidMount() {
+        const cliente = AuthService.getCurrentUser();
+        const idCliente = cliente.id;
+        const tessera = UserService.getTesseraByClienteId(idCliente);
+        const idTessera = tessera.id;
+        fetch("api/tessera/cliente/"+idCliente)
+    .then((res) => res.json())
+           .then((data) => {
+                console.log(data);
+                this.setState({iscrizioni: JSON.stringify(data)}, () => {
+                    alert(this.state.hugeText);
+                });
+
+            });
     }
 
-    render (){
-        const { tessera } = this.state;
-        return (
-            <div className="container">
-                <header className="jumbotron">
-                    <h3>Tessera PF</h3>
-                </header>
-                {tessera && (
-                    <div className="tessera">
-                        <p>Numero Tessera: {tessera.tesseraId}</p>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Programma fedeltà</th>
-                                </tr>
-                            </thead>
-                        <tbody>
-                    {this.state.tessera.iscrizioni.map((subscription) => (
-                        <tr key={subscription.id}>
-                            <td>{subscription.id}</td>
-                            <td>{subscription.programma}</td>
+    render() {
+            return (
+                <div>
+                    <h2>Iscrizioni:</h2>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Id iscrizione</th>
                         </tr>
-                        ))}
+                        </thead>
+                        <tbody>
+                            {this.state.data?.map((iscrizione) => (
+                                <tr key={iscrizione.iscrizioneId}>
+                                    <td>{iscrizione.iscrizioneId}</td>
+                                </tr>
+                            ))}
                         </tbody>
-                        </table>
-                    </div>
-                )}
-            </div>
-        )
+                    </table>
+                </div>
+            );
     }
 }
-//
